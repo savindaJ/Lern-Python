0019a459f0b8dbd33e40f19c6fea91007612198d
savindaJ <your-github-thantrige32@gmail.com.noreply.github.com>
2024-03-30 22:19:51 +0530

    add variables

---
+++ variable/variable.py
@@ -0,0 +1,40 @@
+name = 'savinda'
+print(name)
+
+a = 10
+b = 20
+c = 30
+print((a+b), b, c)
+print(a*b)
+print('division :',a/b)
+print(a//b) # floor division 10/20 = 0.5 but floor division 0
+print('modeul : ',a%b) # modeul
+print(a**b) # power 10^20=100000000000000000000
+print(a+b+c)
+print('savinda'*3) # savindasavindasavinda
+print(int('10')+20) # 10+20=30
+
+
+
+age = 20
+bool = True
+print('my age is',age)
+print(age>18)
+print(age<18)
+
+print('10'=='10') # True is equal any datatype
+
+print(True and False) # 10
+
+print(True or False) # 10
+
+print(not True) # False
+
+print(True ^ False) # True different value is True
+
+x= 10
+x += x
+print(x)
+
+a ,b ,c = 10, 20,'hello'# a=10, b=20
+print(a,b,c)
