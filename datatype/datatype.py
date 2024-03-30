96bb3b4f7c72405eb5709d7e365dec1a520575a2
savindaJ <your-github-thantrige32@gmail.com.noreply.github.com>
2024-03-30 22:00:43 +0530

    add data types

---
+++ datatype/datatype.py
@@ -0,0 +1,15 @@
+print(type(100)) # int
+print(type(100.0)) # float
+print(type("100")) # str
+print(type(True)) # bool
+print(type(100j)) # complex j is imaginary number
+print(type([1, 2, 3])) # list
+print(type((1, 2, 3))) # tuple
+print(type({1, 2, 3})) # set
+print(type({1: "one", 2: "two", 3: "three"})) # dict
+print('welcome to \n python world !') # new line character / escape character
+print('welcome to \t python world !') # tab character / escape character
+print('welcome to \b\b python world !') # backspace character / escape character
+print("hello World !\" hello")
+print("hello World !\' hello")
+print("hello World !\\ hello")
