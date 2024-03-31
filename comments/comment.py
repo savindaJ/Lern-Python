8ecb1beea31306079ce779b3b8c8010ba2710072
savindaJ <your-github-thantrige32@gmail.com.noreply.github.com>
2024-03-31 08:14:24 +0530

    add comments

---
+++ comments/comment.py
@@ -0,0 +1,21 @@
+
+# sinle line comment
+
+''' hwllo
+    multiline comment
+'''
+
+def func () -> int: # type hinting ,  return type is int
+    '''
+    function comment
+    '''
+    pass
+
+x: int = 10 # type hinting
+y: float = 10.5
+z: str = 'hello'
+print(x, y, z)
+
+#TODO: this is todo comment
+#BUG: this is bug comment
+#FIXME: this is fixme comment
