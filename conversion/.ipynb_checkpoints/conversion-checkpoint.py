e30018ccb1633944e283bb5569aa91608ac5578e
savinda <thantrige32@gmail.com>
2025-02-02 10:31:12 +0530

    add input validation and numpy examples to day4 notebook

---
+++ conversion/.ipynb_checkpoints/conversion-checkpoint.py
@@ -0,0 +1,15 @@
+a ='10'
+b = 20
+print(int(a)+b) # 10+20=30
+
+s = 10
+print(str(s)+'20') # 1020
+
+bool = True
+print(int(bool)) # 1
+
+x = input('Enter a number: ')
+print(x)
+
+b = int(input('Enter a number: '))
+print(b)
